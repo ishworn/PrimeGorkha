a603457bcfd26219365ee963897a42fd1731507e
Nirdeshjr <khatiwadanirdesh111@gmail.com>
2025-01-21 00:59:25 +0545

    commit

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,18 +3,18 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "What courier services do you offer?",
+    ans: "We provide reliable and fast delivery services for both local and international shipments. Our services include same-day delivery, next-day delivery, express shipping, and standard shipping options",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How can I track my shipment?",
+    ans: "You can track your shipment by entering the tracking number provided at the time of booking on our tracking page. You'll get real-time updates about your parcel’s status.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How much will my shipment cost?",
+    ans: "Shipping costs depend on the size, weight, destination, and type of service you choose. Use our online price calculator to get an estimate before booking your shipment.",
   },
 ];
 
