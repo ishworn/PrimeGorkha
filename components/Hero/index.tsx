072403ec6e4bad3bde9e46fb7dccc4a7d46ad866
RupeshThakuri <rupeshthakuri16@gmail.com>
2025-01-21 03:03:23 +0545

    p tag

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -18,16 +18,16 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Ready to ship? Let’s make it happen! 
+                🔥 Ready to ship? Let’s make it happen!
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-              Welcome to  {"   "}
+                Welcome to  {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
-                PrimeGorkha
+                  PrimeGorkha
                 </span>
               </h1>
               <p>
-               No matter the size, distance, or destination—trust PrimeGorkha to deliver your parcels with care and precision. Get started now and enjoy the peace of mind that comes with reliable, efficient service!
+                No matter the size, distance, or destination—trust PrimeGorkha to deliver your parcels with care and precision. Get started now and enjoy the peace of mind that comes with reliable, efficient service!
               </p>
 
               <div className="mt-10">
@@ -41,18 +41,14 @@ const Hero = () => {
                       className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
                     />
                     <button
-  aria-label="get started button"
-  className="flex rounded-full bg-[#FFA500] px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-[#FF8C00] dark:bg-[#FF8C00] dark:hover:bg-[#FF7F00]"
->
-  Trace Product
-</button>
-
+                      aria-label="get started button"
+                      className="flex rounded-full bg-[#FFA500] px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-[#FF8C00] dark:bg-[#FF8C00] dark:hover:bg-[#FF7F00]"
+                    >
+                      Trace Product
+                    </button>
                   </div>
                 </form>
 
-                <p className="mt-5 text-black dark:text-white">
-                
-                </p>
               </div>
             </div>
 
