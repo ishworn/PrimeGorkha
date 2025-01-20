ea690cfc1117f69ceecf888b38aef6d6dcdcec9f
Nirdeshjr <khatiwadanirdesh111@gmail.com>
2025-01-21 01:16:53 +0545

    commit

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -18,19 +18,16 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 Ready to ship? Let’s make it happen! 
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+              Welcome to Our Courier Services {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                PrimeGorkha
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+              Whether it's a time-sensitive package or an international shipment, we’ve got you covered with real-time tracking, professional handling, and guaranteed delivery on time, every time. We make shipping easy, convenient, and hassle-free. No matter the size, distance, or destination—trust PrimeGorkha to deliver your parcels with care and precision. Get started now and enjoy the peace of mind that comes with reliable, efficient service!
               </p>
 
               <div className="mt-10">
@@ -40,20 +37,20 @@ const Hero = () => {
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       type="text"
-                      placeholder="Enter your email address"
+                      placeholder="Enter your tracing number"
                       className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
                     />
                     <button
                       aria-label="get started button"
                       className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
                     >
-                      Get Started
+                      Trace Product
                     </button>
                   </div>
                 </form>
 
                 <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
+                
                 </p>
               </div>
             </div>
