5223357e0e6d3c5df54c80efcca2f6c4e8623588
ishwor <ishwornepal1234@gmail.com>
2025-01-24 22:15:26 -0800

    chaneg

--- components/Common/SectionHeader.tsx
+++ components/Common/SectionHeader.tsx
@@ -4,11 +4,12 @@ import { motion } from "framer-motion";
 type HeaderInfo = {
   title: string;
   subtitle: string;
+  description : string;
   
 };
 
 const SectionHeader = ({ headerInfo }: { headerInfo: HeaderInfo }) => {
-  const { title, subtitle,  } = headerInfo;
+  const { title, subtitle, description } = headerInfo;
 
   return (
     <>
@@ -39,7 +40,7 @@ const SectionHeader = ({ headerInfo }: { headerInfo: HeaderInfo }) => {
         <h2 className="mx-auto mb-4 text-3xl font-bold text-black dark:text-white md:w-4/5 xl:w-1/2 xl:text-sectiontitle3">
           {subtitle}
         </h2>
-        <p className="mx-auto md:w-4/5 lg:w-3/5 xl:w-[46%]">{}</p>
+        <p className="mx-auto md:w-4/5 lg:w-3/5 xl:w-[46%]">{description}</p>
       </motion.div>
       {/* <!-- Section Title End --> */}
     </>
