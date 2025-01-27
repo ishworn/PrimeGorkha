d0b2c6384e7df948c652e87b9a12a4ec44bab2ae
RupeshThakuri <rupeshthakuri16@gmail.com>
2025-01-27 13:20:43 +0545

    revert burgher menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -57,7 +57,7 @@ const Header = () => {
           <button
             aria-label="hamburger Toggler"
             className="block xl:hidden"
-            onClick={() => setNavigationOpen((prev) => !prev)} // Toggle the state
+            onClick={() => setNavigationOpen(!navigationOpen)}
           >
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
               <span className="absolute right-0 block h-full w-full">
@@ -67,24 +67,24 @@ const Header = () => {
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-150 duration-200 ease-in-out  ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "delay-400 !w-full" : "w-0"
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-200 duration-200 ease-in-out ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!w-full delay-500" : "w-0"
                   }`}
                 ></span>
               </span>
               <span className="du-block absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-white delay-300 duration-200 ease-in-out  ${
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-[0]" : "h-full"
                   }`}
                 ></span>
                 <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-white duration-200 ease-in-out  ${
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-200" : "h-0.5"
                   }`}
                 ></span>
@@ -121,6 +121,13 @@ const Header = () => {
                       >
                         {menuItem.title}
                         <span>
+                          <svg
+                            className="h-3 w-3 cursor-pointer fill-waterloo group-hover:fill-primary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                          </svg>
                         </span>
                       </button>
 
@@ -149,7 +156,7 @@ const Header = () => {
                               navigationOpen ? "text-black" : "text-white"
                             }`
                       }
-                      onClick={() => setNavigationOpen(false)} // Close the menu when a link is clicked
+                      onClick={() => setNavigationOpen(!navigationOpen)}
                     >
                       {menuItem.title}
                     </Link>
